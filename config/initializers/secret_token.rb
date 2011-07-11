89b68864c68f3961455062e29420a9c39da8b5d5
unnitallman <unni.tallman@gmail.com>
2011-07-11 18:01:27 +0530

    rails app to learn fb graph api

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbModportal::Application.config.secret_token = 'cc6b103980e9199ec75f2bf684a360eee94e4777a616af2bef1d52477424a0c5a8a526fb3f77034da2c5c804cb5e2e8a2614c6a7bce533c6ba3bc715e16dc110'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FbModportal::Application.config.session_store :cookie_store, :key => '_fb_modportal_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FbModportal::Application.config.session_store :active_record_store
