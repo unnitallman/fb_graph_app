6e82d08a4aef68c9c8782bbc270186e11d7b3276
unnitallman <unni.tallman@gmail.com>
2011-07-11 19:43:34 +0530

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   def fb_oauth_url(fb_page)
     fb_auth_response_url = url_for({:host => request.host, :controller => 'fb_pages', 
                                     :action => 'fb_auth_response', :id => fb_page.id})
-    "https://www.facebook.com/dialog/oauth?client_id=#{FB_APP_ID}&redirect_uri=#{fb_auth_response_url}"
+    "https://www.facebook.com/dialog/oauth?client_id=#{FB_APP_ID}&redirect_uri=#{fb_auth_response_url}&scope=offline_access"
   end
   helper_method :fb_oauth_url
 end
