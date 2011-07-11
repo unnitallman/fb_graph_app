93a61986aa9e1dcede83d64953ed84e7c4e83151
Karthik Reddy <karthik@193143-qb0-1.dfw1.staging.slicehost.com>
2011-07-11 13:14:20 +0000

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def fb_oauth_url
-    fb_auth_response_url = url_for({:controller => 'fb_pages', :action => 'fb_auth_response'})
-    "https://www.facebook.com/dialog/oauth?client_id=#{FB_APP_ID}&redirect_uri=#{fb_auth_response_url}
+    fb_auth_response_url = url_for({:host => request.host, :controller => 'fb_pages', :action => 'fb_auth_response'})
+    "https://www.facebook.com/dialog/oauth?client_id=#{FB_APP_ID}&redirect_uri=#{fb_auth_response_url}"
   end
 end
