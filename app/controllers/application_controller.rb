4756376b72a25d322d5e5387805024340265b428
unnitallman <unni.tallman@gmail.com>
2011-07-11 19:32:30 +0530

    fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def fb_oauth_url(fb_page)
+    fb_auth_response_url = url_for({:host => request.host, :controller => 'fb_pages', 
+                                    :action => 'fb_auth_response', :id => fb_page.id})
+    "https://www.facebook.com/dialog/oauth?client_id=#{FB_APP_ID}&redirect_uri=#{fb_auth_response_url}"
+  end
+  helper_method :fb_oauth_url
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,3 @@
 module ApplicationHelper
-  def fb_oauth_url(fb_page)
-    fb_auth_response_url = url_for({:host => request.host, :controller => 'fb_pages', 
-                                    :action => 'fb_auth_response', :id => fb_page.id})
-    "https://www.facebook.com/dialog/oauth?client_id=#{FB_APP_ID}&redirect_uri=#{fb_auth_response_url}"
-  end
+  
 end
